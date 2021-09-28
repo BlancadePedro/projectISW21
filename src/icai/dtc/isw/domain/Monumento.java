aa8cd83817903155c2ebd42317838d0cc55b8335
Blanca de Pedro <blancadepedr@gmail.com>
2021-09-28 18:59:59 +0200

    Sprint 1

---
+++ src/icai/dtc/isw/domain/Monumento.java
@@ -0,0 +1,9 @@
+package icai.dtc.isw.domain;
+public class Monumento extends Localidad{
+
+    public Monumento(String nombre, String direccion, String horario, String contacto, String precio) {
+        super(nombre, direccion, horario, contacto, precio);
+       //No se me ocurre que más poner
+    }
+    
+}
---
+++ src/icai/dtc/isw/domain/Parque.java
@@ -0,0 +1,9 @@
+package icai.dtc.isw.domain;
+public class Parque extends Localidad{
+
+    public Parque(String nombre, String direccion, String horario, String contacto, String precio) {
+        super(nombre, direccion, horario, contacto, precio);
+        
+    }
+    
+}
