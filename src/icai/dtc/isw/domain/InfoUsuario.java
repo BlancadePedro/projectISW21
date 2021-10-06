e2039e3fbdd4f946ec609a26fc2876d0b6ac6e0e
Blanca de Pedro <blancadepedr@gmail.com>
2021-10-06 22:52:43 +0200

    Sprint 1

--- src/icai/dtc/isw/domain/InfoUsuario.java
+++ src/icai/dtc/isw/domain/InfoUsuario.java
@@ -4,17 +4,21 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 
 public class InfoUsuario extends JFrame
 {
-   
+    public static void main(String[] argv){
+        Customer perfil = new Customer("correo", "descripcion", 20, "nacionalidad","nombreCompleto", 646513445, "usuario","clave");
+        new InfoUsuario(perfil);
+
+    }
     private JPanel info = new JPanel(new GridLayout(6, 1));
-    private JPanel foto = new JPanel((new FlowLayout()));
+    private JPanel pnlfoto = new JPanel((new BorderLayout()));
 
     
     private JLabel lblNombre = new JLabel();
@@ -24,12 +28,12 @@ public class InfoUsuario extends JFrame
     private JLabel lblCorreo = new JLabel();
     private JLabel lblTelefono = new JLabel();
 
+    private JComboBox<String> opcionesFotos = new JComboBox<>();
+
     public InfoUsuario(Customer perfil) {
     
         this.setLayout(new BorderLayout());
         this.setVisible(true);
-
-        foto.setBackground(Color.BLACK);
 
         String nombre = perfil.getNombreCompleto();
         int edad = perfil.getEdad();
@@ -52,13 +56,57 @@ public class InfoUsuario extends JFrame
         info.add(lblCorreo);
         info.add(lblTelefono);
 
-        this.add(info,BorderLayout.CENTER);
-        this.add(foto,BorderLayout.WEST);
+        opcionesFotos();
+        pnlfoto.add(opcionesFotos, BorderLayout.NORTH);
+        opcionesFotos.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                int res = opcionesFotos.getSelectedIndex();
+                JLabel foto = new JLabel();
+
+                if(res == 1){
+                    //foto.setIcon(null);
+                    foto.setIcon(new ImageIcon("/home/blanca/IdeaProjects/projectISW19/src/icai/dtc/isw/resources/FotosMapa/Eventos.PNG"));
+                }
+                if(res == 2){
+                    //foto.setIcon(null);
+                    foto.setIcon(new ImageIcon("/home/blanca/IdeaProjects/projectISW19/src/icai/dtc/isw/resources/FotosMapa/Hoteles.PNG"));
+                }
+                if(res == 3){
+                    foto.setIcon(new ImageIcon("/home/blanca/IdeaProjects/projectISW19/src/icai/dtc/isw/resources/FotosMapa/Museo.PNG"));
+                }
+                if(res == 4){
+                    foto.setIcon(new ImageIcon("/home/blanca/IdeaProjects/projectISW19/src/icai/dtc/isw/resources/FotosMapa/Monumentos.PNG"));
+                }
+                if(res == 5){
+                    foto.setIcon(new ImageIcon("/home/blanca/IdeaProjects/projectISW19/src/icai/dtc/isw/resources/FotosMapa/Restaurantes.PNG"));
+                }
+                pnlfoto.add(foto,BorderLayout.CENTER);
+
+            }
+        });
+
+        this.add(info,BorderLayout.EAST);
+        this.add(pnlfoto,BorderLayout.WEST);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
         this.setResizable(true);//Para que no se pueda redimensionar
         this.setLocationRelativeTo(null);
         this.setVisible(true);
+
+
     }
+    public void opcionesFotos(){
+        opcionesFotos.removeAllItems();
+        opcionesFotos.addItem("Personaje 1");
+        opcionesFotos.addItem("Personaje 2");
+        opcionesFotos.addItem("Personaje 3");
+        opcionesFotos.addItem("Personaje 4");
+        opcionesFotos.addItem("Personaje 5");
+
+    }
+
+
     
 }
--- src/icai/dtc/isw/domain/Mapa.java
+++ src/icai/dtc/isw/domain/Mapa.java
@@ -1,4 +1,98 @@
 package icai.dtc.isw.domain;
 
-public class Mapa {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+
+
+
+public class Mapa extends JFrame {
+
+    private static JPanel pnlMapa = new JPanel(new FlowLayout());
+
+    public static void main(String[] argv){
+
+        new Mapa();
+
+    }
+
+
+    
+    public Mapa(){
+        this.setLayout(new BorderLayout());
+        this.setVisible(true);
+        //Compobacion de que funciona-> Lo dejo por si se necesita, pero se podría borrar
+
+        Restaurante restaurante = new Restaurante("nombre","direccion","horario","contacto","precio","tipoComida");
+        Hotel hotel = new Hotel("nombre", "direccion", "horario", "contacto", "precio", "estrellas", "mascotas", "publico");
+        Museo museo = new Museo("nombre", "direccion", "horario", "contacto", "precio","tipoMuseo");
+        Evento evento = new Evento("nombre", "direccion", "precio", "autor", "celebracion", "fecha");
+        Monumento monumento = new Monumento("nombre", "direccion");
+        Parque parque = new Parque("nombre", "direccion");
+
+        botonesMapa(museo, pnlMapa,100,100);
+        this.add(pnlMapa);
+
+
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setResizable(true);//Para que no se pueda redimensionar
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    public static void botonesMapa(Object objects, JPanel panel, int x, int y){
+
+        JButton btn = new JButton();
+
+        if(objects instanceof Restaurante){
+            System.out.println("Restaurante");
+            btn.setIcon(new ImageIcon("/home/blanca/IdeaProjects/projectISW19/src/icai/dtc/isw/resources/FotosMapa/Restaurantes.PNG"));
+        }
+        if(objects instanceof Hotel){
+            System.out.println("Hotel");
+            btn.setIcon(new ImageIcon("/home/blanca/IdeaProjects/projectISW19/src/icai/dtc/isw/resources/FotosMapa/Hoteles.PNG"));
+        }
+        if(objects instanceof Museo){
+            System.out.println("Museo");
+            btn.setIcon(new ImageIcon("/home/blanca/IdeaProjects/projectISW19/src/icai/dtc/isw/resources/FotosMapa/Museo.PNG"));
+        }
+        if(objects instanceof  Monumento){
+            System.out.println("Monumento");
+            btn.setIcon(new ImageIcon("/home/blanca/IdeaProjects/projectISW19/src/icai/dtc/isw/resources/FotosMapa/Monumentos.PNG"));
+        }
+        if(objects instanceof  Parque){
+            System.out.println("Parque");
+            btn.setIcon(new ImageIcon("/home/blanca/IdeaProjects/projectISW19/src/icai/dtc/isw/resources/FotosMapa/Parques.PNG"));
+        }
+        if(objects instanceof  Evento){
+            System.out.println("Evento");
+            btn.setIcon(new ImageIcon("/home/blanca/IdeaProjects/projectISW19/src/icai/dtc/isw/resources/FotosMapa/Eventos.PNG"));
+        }
+
+        btn.setBounds(x,y,5,5);
+        panel.add(btn);
+
+        btn.addActionListener(new ActionListener() {
+            //Prueba para ver si funciona -> Rellenar info base de datos (ALE)
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.out.println(objects.toString());
+            }
+        });
+    }
+
+
+
+
+
+
+    
+
+    
 }
+
