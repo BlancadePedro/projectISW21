d8f5114a5485ed0950e09a68f94a7198a6477f2f
Blanca de Pedro <blancadepedr@gmail.com>
2021-11-06 23:05:53 +0100

    Sprint 2 PrimeraParte

---
+++ src/icai/dtc/isw/domain/PnlMonumentos.java
@@ -0,0 +1,43 @@
+package icai.dtc.isw.domain;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class PnlMonumentos extends JFrame {
+    private JPanel panelCentro = new JPanel((new GridLayout()));
+    private JPanel panelSur = new JPanel((new BorderLayout()));
+    private JButton btnVolver = new JButton("Cerrar");
+
+    public PnlMonumentos(ArrayList<Monumento> lista, Customer perfil){
+
+        int size = lista.size();
+        panelCentro = new JPanel((new GridLayout(size,1)));
+        for (Monumento l : lista) {
+            JLabel label = new JLabel("Nombre: "+l.getNombre()+"; Dirección: "+l.getDireccion());
+            panelCentro.add(label);
+        }
+        btnVolver.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent actionEvent){
+
+                dispose();
+
+            }
+        });
+
+        panelSur.add(btnVolver);
+
+        this.add(panelCentro,BorderLayout.CENTER);
+        this.add(panelSur,BorderLayout.SOUTH);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setResizable(true);//Para que no se pueda redimensionar
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}
